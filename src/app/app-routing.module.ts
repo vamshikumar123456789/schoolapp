3444c38739c593678f35bbfe7fe6529583ed13c9
vamshikumar123456789 <goudvamshikumar88@gmail.com>
2023-07-24 10:41:02 +0530

    authentication guard implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { CreteStudentComponent } from './crete-student/crete-student.component';
 import { AllSudentsComponent } from './all-sudents/all-sudents.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+import { notifyGuard } from './notify.guard';
+import { authenticationGuard } from './authentication.guard';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
-  {path:'dashboard',component:DashboardComponent,children:[
+  {path:'dashboard',component:DashboardComponent,canActivate:[authenticationGuard],children:[
     {path:'home',component:HomeComponent},
-    {path:'crete-student',component:CreteStudentComponent},
-    {path:'all-sudents',component:AllSudentsComponent}
+    {path:'crete-student',component:CreteStudentComponent,canDeactivate:[notifyGuard]},
+    {path:'all-sudents',component:AllSudentsComponent,canDeactivate:[notifyGuard]},
+    {path:'student-details/:id',component:StudentDetailsComponent}
   ]}
 ];
 
