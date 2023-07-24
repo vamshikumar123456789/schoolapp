5183dff2a191db472652bec3553f19d4fc8a265b
vamshikumar123456789 <goudvamshikumar88@gmail.com>
2023-07-24 10:41:44 +0530

    notify guard implemented

---
+++ src/app/notify.guard.ts
@@ -0,0 +1,11 @@
+import { CanDeactivateFn } from '@angular/router';
+
+export const notifyGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
+
+  if(confirm("if you go back your progress will not save! are you willing to go back")){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
